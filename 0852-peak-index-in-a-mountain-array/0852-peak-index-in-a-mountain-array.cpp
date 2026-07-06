e3a053f57b61cdbc5c8769ffa55794b80f142e30
Skadekar2703 <skadekar00@gmail.com>
2026-07-06 11:45:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -8,24 +8,13 @@ public:
         while(low <= high){
             int mid = low + (high - low)/2;
 
-            if((mid != 0) && (mid != n-1)){
-                if((arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1]))     return mid;
-                else if((arr[mid] < arr[mid-1]) && (arr[mid] > arr[mid+1]))     high = mid - 1;
-                else if((arr[mid] > arr[mid-1]) && (arr[mid] < arr[mid+1]))     low = mid + 1;
-
+            if((arr[mid] > arr[mid+1]) && (arr[mid] > arr[mid-1])){
+                return mid;
             }
 
-            else if (mid == 0){
-                if((arr[mid] > arr[mid+1]))     return mid;
-                else if((arr[mid] > arr[mid+1]))     high = mid - 1;
-                else if((arr[mid] < arr[mid+1]))     low = mid + 1;
-            }
+            else if(arr[mid] < arr[mid-1])  low = mid + 1;
+            else     high = mid - 1;
 
-            else if(mid == n-1){
-                if((arr[mid] > arr[mid-1]))     return mid;
-                else if((arr[mid] < arr[mid-1]))     high = mid - 1;
-                else if((arr[mid] > arr[mid-1]))     low = mid + 1;
-            }
 
             
 
