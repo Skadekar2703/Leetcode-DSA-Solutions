a0e5be3869f7098733a722d0882cbdb706ed78e9
Skadekar2703 <skadekar00@gmail.com>
2026-08-07 12:38:15 +0530

    Time: 6 ms (22.13%), Space: 10.2 MB (28.4%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -6,20 +6,25 @@ public:
         string str2 = "";
         int count = 1;
         char ch = str[0];
-        for(int i = 1; i < str.size(); i++){
-            char ch2 = str[i];
-            if(ch == ch2){
+        int i = 1;
+        while(i < str.size()){
+            if(str[i] == ch){
                 count++;
+                i++;
             }
             else{
-                str2 += to_string(count);
+                str2 += (to_string(count));
                 str2 += ch;
                 count = 1;
                 ch = str[i];
+                i++;
             }
+
         }
-        str2 += to_string(count);
+        str2 += (to_string(count));
         str2 += ch;
         return str2;
+        
+        
     }
 };
