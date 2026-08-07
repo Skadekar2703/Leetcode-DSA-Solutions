2e3d50449b9f201af1cbb1850cd011c7590e7757
Skadekar2703 <skadekar00@gmail.com>
2026-08-07 11:05:17 +0530

    Time: 31 ms (5.05%), Space: 20.3 MB (5.2%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<int> ans, vector<vector<int> >& v, int target, int idx, bool flag){
+
+    void helper(vector<int>& v, vector<int> ans, vector<vector<int>>& final, int target, int idx, bool flag){
         if(target == 0){
-            v.push_back(ans);
+            final.push_back(ans);
             return;
         }
         if(target < 0)  return;
-        for(int i = idx; i < nums.size(); i++){
+        for(int i = idx; i < v.size(); i++){
             if(flag){
-                ans.push_back(nums[i]);
-                helper(nums, ans, v, target-nums[i], i+1, flag);
+                ans.push_back(v[i]);
+                helper(v, ans, final, target - v[i], i+1, flag);
                 ans.pop_back();
             }
-            if(i == nums.size()-1)  return;
-            if(nums[i] == nums[i+1])    flag = false;
+            if(i == v.size()-1)   return;
+            if(v[i] == v[i+1])  flag = false;
             else    flag = true;
         }
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> v;
-        vector<int>ans;
-        helper(nums, ans, v, target, 0, true);
-        return v;
+
+    vector<vector<int>> combinationSum2(vector<int>& v, int target) {
+        
+        sort(v.begin(), v.end());
+        vector<int> ans;
+        vector<vector<int>> final;
+        helper(v, ans, final, target, 0, true);
+        return final;
+
     }
 };
