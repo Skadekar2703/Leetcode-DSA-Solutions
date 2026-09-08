df7f3583caa7096dfdcf991c8f05d01691806fc8
Skadekar2703 <skadekar00@gmail.com>
2026-09-08 18:06:54 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& arr) {
-        int n = arr.size();
-        int i = 0;
-        while(i < n){
-            if(arr[i] == i || arr[i] > n-1) i++;
-            else{
-                swap(arr[i] , arr[arr[i]]);
-                
-            }
-        }
-        for(int i = 0; i < n; i ++){
-            if(arr[i] != i) return i;
-        }
-        return n;
+    int missingNumber(vector<int>& v) {
+        int n = v.size();
+        int actual_sum = n*(n+1)/2;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += v[i];
+        } 
+        return (actual_sum - sum);
     }
 };
