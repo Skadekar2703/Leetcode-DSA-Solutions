c71dc2c15e5243fec3ae1a99af7b3f58715e6b21
Skadekar2703 <skadekar00@gmail.com>
2026-09-10 11:01:18 +0530

    Time: 0 ms (100%), Space: 21.5 MB (24.4%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -2,11 +2,50 @@ class Solution {
 public:
     int maximumCount(vector<int>& v) {
         int n = v.size();
+        // int pos = 0, neg = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(v[i]<0)  neg++;
+        //     else if(v[i]>0) pos++;
+        // }
+        // return max(pos, neg);
+
+        int low = 0, high = n-1;
         int pos = 0, neg = 0;
-        for(int i = 0; i < n; i++){
-            if(v[i]<0)  neg++;
-            else if(v[i]>0) pos++;
+        // low se pehle neg hain & high ke baad pos hain
+        int mid = low + (high-low)/2;
+        
+        // small pos nikalo
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(v[mid]>0){
+                pos = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        return max(pos, neg);
+        low = 0, high = n-1;
+
+        while(low<= high){
+            int mid = low + (high-low)/2;
+            if(v[mid]<0){
+                neg = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        // pos tells 1st positive no. index
+        // neg tells 1st negative no. index
+        if(v[pos]>0)   pos = n-pos;
+        else    pos = 0;
+        if(v[neg]<0)    neg = neg+1;
+        else    neg = 0;
+        return max(pos,neg);
+
+
+
     }
 };
