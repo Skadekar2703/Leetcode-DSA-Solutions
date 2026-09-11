ffa1a377a9b824340f2ca164984aa0c50d98498b
Skadekar2703 <skadekar00@gmail.com>
2026-09-11 19:03:41 +0530

    Time: 0 ms (100%), Space: 13.4 MB (29.32%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,12 +4,11 @@ public:
         int n = v.size();
         int low = 0, high = n-1;
         while(low <= high){
-            int mid = low + (high-low)/2;
-            int miss = v[mid] - (mid+1);
-            if(miss < k)     low = mid + 1;
+            int mid = low + (high - low);
+            int miss = v[mid] - (mid + 1);
+            if(k > miss)    low = mid + 1;
             else    high = mid - 1;
         }
-        return (low + k);
-        
+        return low + k;
     }
 };
