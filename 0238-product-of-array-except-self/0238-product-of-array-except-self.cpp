30a4a1b6c5316d3566d9a57be13ffd723fe05856
Skadekar2703 <skadekar00@gmail.com>
2026-09-12 11:45:05 +0530

    Time: 0 ms (100%), Space: 40.3 MB (44.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -9,15 +9,10 @@ public:
             pre[i] = product;
             product *= nums[i];
         }
-        vector<int> suf(n);
         product = 1;
         for(int i = n-1; i >= 0; i--){
-            suf[i] = product;
+            pre[i] *= product;
             product *= nums[i];
-        }
-
-        for(int i = 0; i < n; i++){
-            pre[i] *= suf[i];
         }
         return pre;
         
